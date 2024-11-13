7e19eea8dbcb511b9d74c60dd6214ee983a4e245
Hannes <Hannes.o@t-online.de>
2024-11-13 21:22:17 +0100

    include task-link

--- Blatt04/src/main/java/Main.java
+++ Blatt04/src/main/java/Main.java
@@ -4,26 +4,35 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+
+//https://www.hsbi.de/elearning/data/FH-Bielefeld/lm_data/lm_1360443/homework/sheet04.html
 public class Main {
+
+
   public static void printTree(ASTNode node, String indent, boolean isLast) {
-    // Ausgabe des aktuellen Knotens mit Einrückung und Verzweigungssymbol
+
     System.out.print(indent);
     if (isLast) {
-      System.out.print("L__ "); // Letzter Knoten auf der aktuellen Ebene
+      System.out.print("L__ ");
       indent += "    ";
     } else {
-      System.out.print("|-- "); // Weitere Knoten auf der aktuellen Ebene
+      System.out.print("|-- ");
       indent += "|   ";
     }
     System.out.println(node);
 
-    // Rekursives Durchlaufen der Kinder des aktuellen Knotens
     List<ASTNode> children = node.getChildren();
     for (int i = 0; i < children.size(); i++) {
       printTree(children.get(i), indent, i == children.size() - 1);
     }
   }
 
+  
+  
+  
+  
+  
+  
   public static void main(String... args) throws IOException {
     String input =
         "int f95(int n) {\n"
@@ -40,6 +49,8 @@ public class Main {
             + "\n"
             + "int n = 10;\n"
             + "f95(n);";
+
+
     Blatt04Lexer lexer = new Blatt04Lexer(CharStreams.fromString(input));
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     Blatt04Parser parser = new Blatt04Parser(tokens);
